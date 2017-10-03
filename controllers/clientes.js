234743ff85d3d8f6b76e9eaecbbb2a158c7ff306
joaopnj <joaopnj@yahoo.com.br>
2017-10-03 15:21:28 -0300

    Teste

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -11,17 +11,17 @@ module.exports = (app) => {
                     Cliente.findOne({
                         login : client.email,
                     }, (err, dados) => {
-                        if(!dados){ res.status(400) }
+                        if(!dados){ res.status(400).end(); }
                         else{
                             bcrypt.compare(client.senha, dados.senha, (err, callback) => {
                                 if(err) console.log(err);
-                                if(!callback){ res.status(400)}
+                                if(!callback){ res.status(400).end();}
                                 else{
                                     bcrypt.genSalt(5, (err, salt) => {
                                     if (err) console.log(err);
                                         bcrypt.hash(dados.login, salt, null, (err, hash) => {
                                             if (err) console.log(err);
-                                            res.status(200);
+                                            res.status(200`).end();
                                         });
                                     });
                                 }
