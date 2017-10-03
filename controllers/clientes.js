77ec625b1c08f94917b25961e1cf7a42376e9df7
joaopnj <joaopnj@yahoo.com.br>
2017-10-03 10:52:34 -0300

    Login via app funcionando.

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -11,22 +11,22 @@ module.exports = (app) => {
                     Cliente.findOne({
                         login : client.email,
                     }, (err, dados) => {
-                            if(!dados){ res.status(400).send('O email não existe'); }
-                            else{
-                                bcrypt.compare(client.senha, dados.senha, (err, callback) => {
-                                    if(err) console.log(err);
-                                    if(!callback){ res.status(400).send('O senha não existe'); }
-                                    else{
-                                        bcrypt.genSalt(5, (err, salt) => {
+                        if(!dados){ res.status(400).send('O email não existe'); }
+                        else{
+                            bcrypt.compare(client.senha, dados.senha, (err, callback) => {
+                                if(err) console.log(err);
+                                if(!callback){ res.status(400).send('O senha não existe'); }
+                                else{
+                                    bcrypt.genSalt(5, (err, salt) => {
+                                    if (err) console.log(err);
+                                        bcrypt.hash(dados.login, salt, null, (err, hash) => {
                                             if (err) console.log(err);
-                                            bcrypt.hash(dados.login, salt, null, (err, hash) => {
-                                                if (err) console.log(err);
-                                                res.status(200).send('Login efetuado com sucesso! ');
-                                            });
+                                            res.status(200).send('Login efetuado com sucesso! ');
                                         });
-                                    }
-                                });
-                            }
+                                    });
+                                }
+                            });
+                        }
                     });
                 }	
                 else{	
